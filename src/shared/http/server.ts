3972ca5320ff767c8d65bfe9a89dd963078e5b08
André C. Scherrer <andrescherrer@gmail.com>
2024-02-07 16:48:37 -0300

    add dotenv

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,10 @@
+import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
-
 app.use(express.json());
 
 app.get('/heathcheck', (request, response) => {
@@ -22,6 +22,6 @@ app.get('/heathcheck', (request, response) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}!`);
 });
