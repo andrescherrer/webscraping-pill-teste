412a1a84295d355c3c2e1fd732103ef8ce9e1407
André C. Scherrer <andrescherrer@gmail.com>
2024-02-07 17:07:34 -0300

    add errors middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,11 @@
+import { AppError } from '@shared/errors/AppError';
 import { Router } from 'express';
 
 const routes = Router();
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'OK!' });
+});
 
 routes.get('/heathcheck', (request, response) => {
   const heathcheck = {
