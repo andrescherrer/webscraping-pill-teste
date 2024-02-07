613141cfe6ab0d14fe5aae9da13c2e1a8097b3da
André C. Scherrer <andrescherrer@gmail.com>
2024-02-07 16:10:48 -0300

    add express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,27 @@
-export class AppServer {
-  private app: string;
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
 
-  constructor(info: string) {
-    this.app = info ?? 'Olá Dev';
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/heathcheck', (request, response) => {
+  const heathcheck = {
+    uptime: process.uptime(),
+    message: 'OK!',
+    timestamp: Date.now(),
+  };
+
+  try {
+    response.send(heathcheck);
+  } catch (error) {
+    heathcheck.message = error;
+    response.status(503).send();
   }
-}
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000!');
+});
